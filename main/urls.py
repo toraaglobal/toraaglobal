d174371db03bd76e8629755c65e1d2de3fdfec49
Tajudeen Abdulazeez <tabdulazeez99@gmail.com>
2022-07-04 01:51:18 -0700

    modidied template

--- main/urls.py
+++ main/urls.py
@@ -20,6 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import HomePageView
 
+admin.site.site_header = 'Toraaglobal'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('',HomePageView.as_view(),name= 'home'),
